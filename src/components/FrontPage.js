950e19f61dbff40837d5a6fe2e7f08218b69f56e
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-07-20 15:21:15 +0100

    Added personal image to home page

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,7 +1,15 @@
 import React from "react"
+import styled from "styled-components"
+import { StaticImage } from "gatsby-plugin-image"
+
+const Container = styled.div`
+  height: 100vh;
+`
 
 const FrontPage = () => (
-  <div>Frontpage</div>
+  <Container>
+    <StaticImage src="../images/personal.jpeg" alt="" />
+  </Container>
 )
 
 export default FrontPage
