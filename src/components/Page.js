6795d5f0fdfc8fe723f9974b6fd2e1cd24e6cdf8
Samuel Slade <samuel.pjm.slade@gmail.com>
2021-12-22 13:43:56 +0000

    Upgraded packages to latest versions

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,15 +1,12 @@
 import React from "react"
-import styled from "styled-components"
 import GlobalStyles from "src/components/GlobalStyles"
 
-const Container = styled.div``
-
 const Page = ({ children }) => (
-  <Container>
+  <>
     <GlobalStyles />
 
     {children}
-  </Container>
+  </>
 )
 
 export default Page
